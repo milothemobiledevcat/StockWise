cdcc65b42e2cdf0342690cd68b4d1e31c596f069
lawrence Quijano <lawrencequijano222@gmail.com>
2026-07-15 09:40:05 -0700

    inventory-ui-1.0

--- app/src/main/java/com/example/stockwise/InventoryActivity.kt
+++ app/src/main/java/com/example/stockwise/InventoryActivity.kt
@@ -1,5 +1,6 @@
 package com.example.stockwise
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,10 +8,16 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class InventoryActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Enables edge-to-edge display (status bar/navigation bar transparency)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_inventory)
+
+        // This ensures your UI content doesn't get covered by system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
